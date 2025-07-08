9ed19e128f2c102544033865bf06f9351d8d4415
toydotgame <toydotgame@gmail.com>
2025-07-08 21:23:53 +1000

    [Blog] History of the Eclipse IDE Splash Screen

--- media/js/contentloader.js
+++ media/js/contentloader.js
@@ -25,11 +25,11 @@ var recentblogs = `
 <div id="recentblogs">
 <h1>Recent Blog Posts</h1>
 <ul>
+	<li><a href="/blog/2025/07-08_eclipse-splash-history"><b>2025-07-08:</b> History of the Eclipse IDE Splash Screen</a></li>
 	<li><a href="/blog/2025/01-18_how-to-downgrade-from-ios-9-to-6"><b>2025-01-18:</b> How to Downgrade from iOS 9 to iOS 6</a></li>
 	<li><a href="/blog/2024/07-23_mbs"><b>2024-07-23:</b> The Tale of Minecraft's Baddest Server</a></li>
 	<li><a href="/blog/2024/06-17_versioning"><b>2024-06-17:</b> toy's Standardised Versioning</a></li>
 	<li><a href="/blog/2024/01-03_fs-measurements"><b>2024-01-03:</b> Digital Metric Unit Nomenclature for Dummies</a></li>
-	<li><a href="/blog/2024/01-01_web-design"><b>2024-01-01:</b> What's the deal with Modern Web Design?</a></li>
 </ul>
 </div>
 `;
